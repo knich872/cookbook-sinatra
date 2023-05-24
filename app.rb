7e7ec08ab2062c4615cf794b76608cb01cd302fc
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-05-24 18:01:24 +0900

    Sinatra boilerplate app

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
-require "better_errors"
+# require "better_errors"
 
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path(__dir__)
-end
+# configure :development do
+#   use BetterErrors::Middleware
+#   BetterErrors.application_root = File.expand_path(__dir__)
+# end
 
 get "/" do
   "Hello world!"
